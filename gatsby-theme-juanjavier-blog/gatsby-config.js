999f6bf15359f5cf3705057d672a71424ea892a6
JuanJavier1979 <jjmrestituto@gmail.com>
2019-10-27 14:04:39 +0100

    [Update] Dependencies: removed sass, added posts [Add] SiteMetadata to Gatsby Theme [Add] Gatsby Theme Plugin: PostCSS [Add] Gatsby Theme Plugin: Source Filesystem

--- gatsby-theme-juanjavier-blog/gatsby-config.js
+++ gatsby-theme-juanjavier-blog/gatsby-config.js
@@ -1,1 +1,19 @@
-module.exports = {}
+module.exports = {
+  siteMetadata: {
+    title: `JuanJavier's Blog Gatsby Theme`,
+    description: `A simple and minimalistic Gatsby Blog Theme for JuanJavier's Blog Site.`,
+    author: `@JuanJavier1979`,
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ]
+}
